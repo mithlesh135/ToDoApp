0f2dd7f658c4eaa8ac435d9816336551d7dc6e66
Singh, Mithlesh <Mithlesh.Singh@cdk.com>
2019-07-30 16:50:08 +0530

    first commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (<div>Hello React</div>);
+}
+
+ReactDOM.render(<App/>, document.getElementById("app-container"));
+
+
+
