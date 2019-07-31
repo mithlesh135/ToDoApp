e1921ef58f663c75e87b5adf812b16dd0703cd9e
Singh, Mithlesh <Mithlesh.Singh@cdk.com>
2019-07-31 14:22:41 +0530

    wiring compoenents

--- app/index.js
+++ app/index.js
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import List from "./list";
+import Add from "./add";
 
-function App() {
-    return (<div><List/></div>);
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        };
+    }
+
+    addItem(currentItem) {
+        this.setState(prevState => {
+            return {
+                data: [...prevState.data, currentItem]
+            }
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Add add={this.addItem.bind(this)}/>
+                <List data={this.state.data}/>
+            </React.Fragment>
+        );
+    }
 }
 
 ReactDOM.render(<App/>, document.getElementById("app-container"));
